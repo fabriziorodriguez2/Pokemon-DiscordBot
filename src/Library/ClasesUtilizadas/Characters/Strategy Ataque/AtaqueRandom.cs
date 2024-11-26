7802f91fdb8a3de1fb5608ea5179db9fa656f5bc
Constanza Blanco <constanza.blanco@correo.ucu.edu.uy>
2024-11-26 16:36:22 -0300

    Update AtaqueRandom.cs

--- src/Library/ClasesUtilizadas/Characters/Strategy Ataque/AtaqueRandom.cs
+++ src/Library/ClasesUtilizadas/Characters/Strategy Ataque/AtaqueRandom.cs
@@ -1,10 +1,1 @@
-﻿namespace Ucu.Poo.DiscordBot.ClasesUtilizadas.Characters.Strategy_Ataque;
 
-public class AtaqueRandom:IAtaqueDanioStrategy
-{
-    public int GetNumero()
-    {
-        Random random = new Random();
-        return random.Next(10);
-    }
-}
